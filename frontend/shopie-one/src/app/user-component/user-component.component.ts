b10503133784642d96b62d80605bb4141c2440f2
jonnie254 <johnkamanu254@proton.me>
2024-07-01 12:01:56 +0300

    addedc code

---
+++ frontend/shopie-one/src/app/user-component/user-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-component.component.html',
+  styleUrl: './user-component.component.css'
+})
+export class UserComponentComponent {
+
+}
