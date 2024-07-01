c3a20b1f7fd0354b59bd6cdce0dd623d69597eff
jonnie254 <johnkamanu254@proton.me>
2024-07-01 13:34:26 +0300

    ADDED CODE

--- frontend/shopie-one/src/app/app.routes.ts
+++ frontend/shopie-one/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { SidebarComponent } from './admin-component/sidebar/sidebar.component';
+import { LoginComponent } from './admin-user/login/login.component';
+import { ProductsComponent } from './admin-component/products/products.component';
+import { CustomerComponent } from './admin-component/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'admin',
+    component: SidebarComponent,
+    children: [
+      { path: 'product', component: ProductsComponent },
+      { path: 'customer', component: CustomerComponent },
+    ],
+  },
+];
